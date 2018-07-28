39d68cdbaf5604cb44f3fc5c904f53a26a454f4d
zjh514514 <zjh514514@qq.com>
2018-07-29 00:27:27 +0800

    Ex9

---
+++ src/code/Ex9.java
@@ -0,0 +1,57 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 9.回文数
+ */
+class Solution {
+    public boolean isPalindrome(int x) {
+        /**
+         * 反转一半数字比较
+         */
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        if (x < 10) {
+            return true;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == revertedNumber || x == revertedNumber / 10;
+        /**
+         * 转为String后比较
+         */
+//        String strX = Integer.toString(x);
+//        String strR = new StringBuilder(strX).reverse().toString();
+//        return strX.equals(strR);
+    }
+}
+
+public class Ex9 {
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int x = Integer.parseInt(line);
+
+            boolean ret = new Solution().isPalindrome(x);
+
+            String out = booleanToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
